2d852f6c6d93993bb5e485a5a8d397bf90831b7f
danilo targas <danilo@adalov.com>
2020-06-19 10:30:41 -0300

    projeto iniciado

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">
+          Home
+          </a>
+      </header>
+
+      <main>
+        Produtos
+      </main>
+    </div>
+  );
+}
+
+export default App;
